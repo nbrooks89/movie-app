8880ce98c11118e5bfec96134e54cc83e9f55e5c
Nicole Brooks <nbrooks2389@gmail.com>
2020-11-21 13:53:51 -0500

    import thumb up thumb down for thumbs up thubs down functionality

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./MovieCard.css";
-import NoPicture from "./No_Picture.jpg";
+import NoPicture from "../../assets/No_Picture.jpg";
 
 function MovieList({ title, id, poster, imgUrl }) {
   return (
     <div className="cardDetailsContainer">
-      <Link to={"/Movie/" + id} title1={title}>
+      <Link to={"/Movie/" + id}>
         <div className="movieTitle">{title}</div>
         <img src={poster ? imgUrl + poster : NoPicture} alt={title} key={id} />
       </Link>
--- src/Pages/MovieDetails/MovieDetails.js
+++ src/Pages/MovieDetails/MovieDetails.js
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import NoPicture from "../../assets/No_Picture.jpg";
+import "./MovieDetails.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+import { faThumbsDown } from "@fortawesome/free-solid-svg-icons";
+function MovieDetails({ match }) {
+  const [movieDetails, setMovieDetails] = useState([]);
+  const [cast, setCast] = useState([]);
+  const [favorites, setFavorites] = useState([
+    { title: movieDetails.title, thumpsup: 0, thumbsDown: 0 },
+  ]);
+  console.log(favorites);
+  const handleGetDetails = () => {
+    const key = process.env.REACT_APP_MOVIE_API_KEY;
+    const url = `https://api.themoviedb.org/3/movie/${match.params.id}?api_key=${key}&append_to_response=credits&page=1`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => {
+        setMovieDetails(res);
+        setCast(res.credits.cast);
+      });
+  };
 
-function MovieDetails({ title }) {
+  const addFav = (props) => {};
+  useEffect(() => {
+    handleGetDetails();
+    console.log(movieDetails);
+    console.log(cast);
+  }, []);
+
   return (
-    <div>
-      <h1>{title}</h1>
+    <div className="movieDetailsContainer">
+      <div className="detailsInnerContainer">
+        <img
+          src={
+            movieDetails.poster_path
+              ? "https://image.tmdb.org/t/p/w400" + movieDetails.poster_path
+              : NoPicture
+          }
+          alt={movieDetails.title}
+        />
+        <h1>{movieDetails.title}</h1>
+        <div>{movieDetails.release_date}</div>
+        <div>{movieDetails.overview}</div>
+
+        {favorites.includes(i) &&
+          <FontAwesomeIcon
+            onClick={() => addFav({ items, i })}
+            icon={faThumbsUp}
+            size="3x"
+            color="rgb(169, 169, 177)"
+}
+      </div>
     </div>
   );
 }
